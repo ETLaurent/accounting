a549d5c6293d610c30234caa742ad26a899aedea
Etienne Laurent <git.sabbath464@passmail.net>
2025-01-17 04:20:04 +0100

    check ansi support

--- utils/string.py
+++ utils/string.py
@@ -1,5 +1,18 @@
+import os
+import sys
+
+
+def supports_ansi():
+    if not sys.stdout.isatty():
+        return False
+    term = os.getenv("TERM", "")
+    return "xterm" in term or "ansi" in term or "screen" in term
+
+
 def style(text, ansi_code):
-    return ''.join(ansi_code + c + '\033[0m' for c in text)
+    if supports_ansi():
+        return ''.join(ansi_code + c + '\033[0m' for c in text)
+    return text
 
 
 def bold(text):
