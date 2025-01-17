6a3a1b6472383921b9d9857e4106821fcecb5c4e
Etienne Laurent <git.sabbath464@passmail.net>
2025-01-17 03:49:20 +0100

    add bold and dim utils

--- utils/string.py
+++ utils/string.py
@@ -1,6 +1,14 @@
-def strikethrough(text):
-    return ''.join(c + '\u0336' for c in text)
+def bold(text):
+    return ''.join('\033[1m' + c + '\033[0m' for c in text)
+
+
+def dim(text):
+    return ''.join('\033[2m' + c + '\033[0m' for c in text)
 
 
 def italic(text):
     return ''.join('\033[3m' + c + '\033[0m' for c in text)
+
+
+def strikethrough(text):
+    return ''.join(c + '\u0336' for c in text)
